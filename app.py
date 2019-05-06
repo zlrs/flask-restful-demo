06d22915914a4992722a8c6b525b3a64b218eb6e
Zhu Yuanqing <zhuys123@gmail.com>
2019-05-07 01:38:41 +0800

    pass

--- app.py
+++ app.py
@@ -11,16 +11,21 @@ city_fields = {
     'Name': fields.Raw,
     'Population': fields.Raw,
     'CountryCode': fields.Raw,
-    # 'District': fields.Raw,
+    'District': fields.Raw,
 }
+# {'_sa_instance_state': <sqlalchemy.orm.state.InstanceState object at 0x0000025D3393B0F0>,
+# 'ID': 1, 'District': 'Kabol', 'Name': 'Kabul', 'Population': 1780000, 'CountryCode': 'AFG'}
 
 
 class Cities(Resource):
     @marshal_with(city_fields)
     def get(self):
         session = DBSession()
-        cities = session.query(City).all()
-        cities_dict = [e.__dict__ for e in cities]
+        cities = session.query(City).limit(20).all()  # list
+        cities_dict = [e.__dict__ for e in cities]    # list
+        print(cities[0])
+        print(dir(cities[0]))
+        print(cities_dict[0])
         return cities_dict
 
 
